a187049acf1bb15c9d3f2b897ca9f0842ee9939d
Ravi kumar Bhartiya <rkseven7@hotmail.com>
2025-09-20 00:06:44 +0530

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-// basic startup: AOS (animate on scroll)
-document.getElementById('year').textContent = new Date().getFullYear();
-AOS.init({ duration: 700, once: true });
-
-// (optional) hook to change contact info easily
-// document.getElementById('contact-phone').textContent = "+91-90000-00000";
